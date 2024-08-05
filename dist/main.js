da8d780140934e12637e359d4a02d08c2d316965
Anastasiia Pavliukova <anastasiapavliukova19@gmail.com>
2024-08-05 12:00:52 +0300

    add main.js for filters sidebar mobile

---
+++ dist/main.js
@@ -0,0 +1,23 @@
+/* ========== catalog filters open for mobile ========== */
+
+const filtersOpenBtn = document.getElementById('catalog_filters_btn');
+const filtersSidebar = document.getElementById('filters_sidebar');
+const filtersCloseBtn = document.getElementById('filters_close');
+const filtersOpenOverlay = document.querySelector('.filters-sidebar__overlay');
+const filtersBackBtn = document.querySelector('.filters-sidebar__back');
+
+filtersOpenBtn.addEventListener('click', () => {
+    filtersSidebar.style.display = 'flex';
+});
+
+filtersCloseBtn.addEventListener('click', () => {
+    filtersSidebar.style.display = 'none';
+});
+
+filtersOpenOverlay.addEventListener('click', () => {
+    filtersSidebar.style.display = 'none';
+});
+
+filtersBackBtn.addEventListener('click', () => {
+    filtersSidebar.style.display = 'none';
+});
