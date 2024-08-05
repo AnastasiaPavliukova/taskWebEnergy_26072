164ef156b97840206df876a6a2e25a6d6532ed2d
Anastasiia Pavliukova <anastasiapavliukova19@gmail.com>
2024-08-05 12:10:45 +0300

    add translate for filters sidebar (mobile)

--- dist/main.js
+++ dist/main.js
@@ -7,17 +7,18 @@ const filtersOpenOverlay = document.querySelector('.filters-sidebar__overlay');
 const filtersBackBtn = document.querySelector('.filters-sidebar__back');
 
 filtersOpenBtn.addEventListener('click', () => {
-    filtersSidebar.style.display = 'flex';
+    filtersSidebar.style.transform = 'translateX(0)'
 });
 
 filtersCloseBtn.addEventListener('click', () => {
-    filtersSidebar.style.display = 'none';
+   filtersSidebar.style.transform = 'translateX(-1000px)'
+    
 });
 
 filtersOpenOverlay.addEventListener('click', () => {
-    filtersSidebar.style.display = 'none';
+    filtersSidebar.style.transform = 'translateX(-1000px)'
 });
 
 filtersBackBtn.addEventListener('click', () => {
-    filtersSidebar.style.display = 'none';
+   filtersSidebar.style.transform = 'translateX(-1000px)'
 });
