67c9de3e3f3750150e64dd9490e0084baf6e2db6
Anastasiia Pavliukova <anastasiapavliukova19@gmail.com>
2024-08-07 16:57:10 +0300

    add sort feature

--- dist/js/catalog.js
+++ dist/js/catalog.js
@@ -1,10 +1,40 @@
 const cardTemplate = document.querySelector('.catalog-card').cloneNode(true);
 const cardsRoot = document.querySelector('.catalog-cards');
 
+const catalogSort = document.getElementById('catalog_sort');
+
+const defaultSort = 'price-acsending';
+let actualSort = '';
+
+let actualCardsData;
+
+const sortFunctions = {
+    'price-acsending' : ((a, b) => {
+        return a.price - b.price;
+     }),
+    'price-descending' : ((a, b) => {
+        return b.price - a.price;
+     })
+}
+
+catalogSort.onchange = () => {
+    actualSort = catalogSort.value;
+    renderCatalog(actualCardsData);
+}
+ 
+function sortCardsData(cardsData) {
+    const sortFunction = sortFunctions[actualSort] || sortFunctions[defaultSort];
+    cardsData.sort(sortFunction);
+}
+
 function renderCatalog(cardsData) {
+    actualCardsData = cardsData;
+
+    sortCardsData(cardsData);
+
     const newCards = [];
-    
-    for (const cardData of cardsData ) {
+
+    for (const cardData of cardsData) {
 
         const newCard = cardTemplate.cloneNode(true);
 
